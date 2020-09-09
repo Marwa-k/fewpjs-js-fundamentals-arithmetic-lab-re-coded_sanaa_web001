8b5eae421c0fb808afbd0f7cf6ed524183e0b515
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-09 22:46:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 // Write your code here
 
+<<<<<<< HEAD
  let num1=31;
  let num2=2;
  
@@ -13,3 +14,11 @@
  let mod  =109 % 21
  
  let max= Math.max(10,9,20,7)
+=======
+Function multiply(num1 ,num2)
+{
+  return num1 *num2;
+}
+
+ multiply(31,2)
+>>>>>>> 039ba23eb9ff8e45fca92affd6d80935f26a2600
